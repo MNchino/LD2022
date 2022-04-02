a52f324c51623bcc3ba53f24b47f9275ce4ceabb
Anthony Ellis <sn.trett@gmail.com>
2022-04-02 16:10:56 -0400

    player movement

---
+++ The Entity/Player/Player.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+var motion_velocity : Vector2  = Vector2(0,0)
+var motion_accel : float = .4
+var motion_frict : float = .4
+var input_speed : Vector2 = Vector2(100,100)
+var max_velocity : Vector2 = Vector2(400,400)
+var dir : Vector2 = Vector2(1,0) 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	if GInput.dir.x != 0:
+		dir.x = GInput.dir.x
+		motion_velocity.x += ( dir.x * input_speed.x ) * motion_accel
+	else:
+		motion_velocity.x = lerp(motion_velocity.x, 0, motion_frict)
+		
+	if GInput.dir.y != 0:
+		dir.y = GInput.dir.y
+		motion_velocity.y += ( dir.y * input_speed.y ) * motion_accel
+	else:
+		motion_velocity.y = lerp(motion_velocity.y, 0, motion_frict)
+	
+	motion_velocity.x = clamp(motion_velocity.x, -max_velocity.x, max_velocity.x)
+	motion_velocity.y = clamp(motion_velocity.y, -max_velocity.y, max_velocity.y)
+	
+	move_and_slide(motion_velocity)
+
---
+++ The Entity/Singletons/GInput.gd
@@ -0,0 +1,51 @@
+extends Node
+
+
+var _left_input : int = 0
+var _right_input : int = 0
+var _up_input : int = 0
+var _down_input : int = 0
+var xdir : int = 0 setget , get_xdir
+var ydir : int = 0 setget , get_ydir
+var dir : Vector2 = Vector2(1,0) setget , get_dir
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _input(event : InputEvent):
+	if event.is_action("game_left"):
+		if event.is_action_pressed("game_left"):
+			_left_input =  -1
+		elif event.is_action_released("game_left"):
+			_left_input = 0
+	if event.is_action("game_right"):
+		if event.is_action_pressed("game_right"):
+			_right_input =  1
+		elif event.is_action_released("game_right"):
+			_right_input = 0
+	if event.is_action("game_up"):
+		if event.is_action_pressed("game_up"):
+			_up_input =  -1
+		elif event.is_action_released("game_up"):
+			_up_input = 0
+	if event.is_action("game_down"):
+		if event.is_action_pressed("game_down"):
+			_down_input =  1
+			if self.ydir > 0:
+				emit_signal("down_pressed")
+		elif event.is_action_released("game_down"):
+			_down_input = 0
+			
+#########################################################################################
+
+func get_xdir():
+	return _left_input + _right_input
+	
+func get_ydir():
+	return _up_input + _down_input 
+	
+func get_dir():
+	return Vector2(self.xdir, self.ydir)
