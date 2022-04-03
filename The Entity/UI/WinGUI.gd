562ea8f948a601bc434e422f5a84ba2ab5408ff2
Anthony Ellis <sn.trett@gmail.com>
2022-04-03 14:00:22 -0400

    Displays time taken

--- The Entity/UI/WinGUI.gd
+++ The Entity/UI/WinGUI.gd
@@ -6,6 +6,7 @@ func _ready():
 
 func show_after_win(player_that_died: Player):
 	visible = true
+	$Panel/VBoxContainer/HBoxContainer/TimeInSeconds.text = "%10.2f seconds" % GameState.time_taken
 
 func _on_ResetButton_pressed():
 	GameState.reset()
