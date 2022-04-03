b282f4948fb61dec9891a56dfb08f16201d5d3cb
Anthony Ellis <sn.trett@gmail.com>
2022-04-02 22:44:33 -0400

    KNOCKY BACKY THE ENEMY UWU

--- The Entity/Entity/Entity.gd
+++ The Entity/Entity/Entity.gd
@@ -6,6 +6,9 @@ var motion_velocity : Vector2  = Vector2(0,0)
 var motion_accel : float = .4
 var input_speed : Vector2 = Vector2(100,100)
 var max_velocity : Vector2 = Vector2(400,400)
+var knocked_back : bool = false
+var knock_back_dir : Vector2 = Vector2(0,0)
+var knock_back_speed : float = 300
 
 func _ready():
 	GameState.connect("player_spawned", self, "start_player_follow")
@@ -14,15 +17,34 @@ func _physics_process(delta):
 	
 	#TODO Convert to NavMesh Later
 	if active:
-		var dir_to_player = (GameState.cur_player.global_position - global_position).normalized()
+		if knocked_back:
+			motion_velocity = knock_back_speed * knock_back_dir
+		else:
+			var dir_to_player = (GameState.cur_player.global_position - global_position).normalized()
+				
+			motion_velocity.x += ( dir_to_player.x * input_speed.x ) * motion_accel
+			motion_velocity.y += ( dir_to_player.y * input_speed.y ) * motion_accel
 			
-		motion_velocity.x += ( dir_to_player.x * input_speed.x ) * motion_accel
-		motion_velocity.y += ( dir_to_player.y * input_speed.y ) * motion_accel
-		
-		motion_velocity.x = clamp(motion_velocity.x, -max_velocity.x, max_velocity.x)
-		motion_velocity.y = clamp(motion_velocity.y, -max_velocity.y, max_velocity.y)
-		
+			motion_velocity.x = clamp(motion_velocity.x, -max_velocity.x, max_velocity.x)
+			motion_velocity.y = clamp(motion_velocity.y, -max_velocity.y, max_velocity.y)
+			
 		move_and_slide(motion_velocity)
 	
 func start_player_follow(player : Player):
 	active = true
+
+func knock_back(away_from_position : Vector2):
+	if !knocked_back:
+		knocked_back = true
+		knock_back_dir = (global_position - away_from_position).normalized()
+		$KnockbackTime.start()
+		print('KNOCKEM')
+
+func _on_KnockbackTime_timeout():
+	knocked_back = false
+
+
+func _on_Hurtbox_area_entered(area):
+	print('KNOCKEM')
+	knock_back(area.global_position)
+	area.queue_free()
--- The Entity/Player/Attack/Attack.gd
+++ The Entity/Player/Attack/Attack.gd
@@ -1,4 +1,7 @@
-extends Node2D
+extends Area2D
+
+func _ready():
+	$DestructionTimer.start()
 
 func _on_DestructionTimer_timeout():
 	queue_free()
