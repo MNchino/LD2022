2e22c23a8e1e3551a298a2da4be978e0b793830a
Kaiera Romero <fia.kaiera@gmail.com>
2022-04-09 23:11:45 +0800

    speaking of redundancies

--- The Entity/Level/Intro/IntroLabelDashRefresh.gd
+++ The Entity/Level/Intro/IntroLabelDashRefresh.gd
@@ -1,17 +1,9 @@
-extends Area2D
+extends "res://Level/Intro/IntroLabel.gd"
 
 var first : bool = false
-
-func _ready():
-	# warning-ignore:return_value_discarded
-	GameState.connect("player_died", self, "_hide_time")
-	$Label.modulate = Color(1,1,1,0)
 
 func _on_IntroLabel_body_entered(_body):
 	if !first:
 		first = true
 		$AnimationPlayer.play("FadeIn")
 	GameState.reset_dashes()
-
-func _hide_time(_player):
-	hide()
