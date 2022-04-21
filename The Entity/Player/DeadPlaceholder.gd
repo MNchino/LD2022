24ddb3725181bffc448a7072a2e22f2d5ae58d4d
Anthony Ellis <sn.trett@gmail.com>
2022-04-20 20:38:30 -0400

    hide the body

--- The Entity/Player/DeadPlaceholder.gd
+++ The Entity/Player/DeadPlaceholder.gd
@@ -4,12 +4,18 @@ var secondframe : bool = false
 var xcy_frames = preload("res://Sprite/Player/XcyFrames.tres")
 
 func _ready():
+	GameState.connect("player_spawned", self, 'hide_body_from_light')
 	GameState.camera.target = self
 	$PlayerSprite.play()
 	$Audio/DeadHit.play()
 	GameState.freeze(0.2)
 	if get_tree().get_current_scene().name == "XcyPlayspace":
 		$PlayerSprite.frames = xcy_frames
+		
+func hide_body_from_light(_player):
+	if GameState.is_snow_mode:
+		$PlayerSprite.material = null
+		$PlayerSprite.light_mask = 1
 	
 func _process(_delta):
 	if $PlayerSprite.frame == 0:
